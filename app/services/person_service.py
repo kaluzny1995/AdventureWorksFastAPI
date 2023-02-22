93c91f1dae6a977af5823a93f92a978592af2640
kaluzny1995 <dzh.awaria@gmail.com>
2023-02-22 22:26:57 +0100

    CORS enabling

--- app/services/person_service.py
+++ app/services/person_service.py
@@ -10,7 +10,9 @@ class PersonService:
     def get_persons_by_phrases(self, first_name_phrase: Optional[str], last_name_phrase: Optional[str]):
         if first_name_phrase is None and last_name_phrase is None:
             raise errors.EmptyFieldsError()
+
         all_persons = self.person_provider.get_persons()
+
         if first_name_phrase is not None and last_name_phrase is not None:
             relevant_persons = list(filter(
                 lambda p: first_name_phrase.lower() in p.first_name.lower() and
@@ -19,4 +21,8 @@ class PersonService:
             relevant_persons = list(filter(lambda p: first_name_phrase.lower() in p.first_name.lower(), all_persons))
         else:
             relevant_persons = list(filter(lambda p: last_name_phrase.lower() in p.last_name.lower(), all_persons))
+
+        relevant_persons = list(sorted(relevant_persons,
+                                       key=lambda rp: (rp.last_name, rp.first_name, rp.middle_name or "")))
+
         return relevant_persons
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError, StarletteHTTPException
 
 from app.routes import main_router, table_router, person_router, phone_number_type_router, person_phone_router
@@ -6,6 +7,16 @@ from app.error_handlers import custom_http_error_handler, custom_request_validat
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(main_router)
 app.include_router(table_router)
