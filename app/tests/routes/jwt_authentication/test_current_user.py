5c1f582b08e3add185d8840a1e722331ca7488eb
kaluzny1995 <dzh.awaria@gmail.com>
2025-02-14 00:53:01 +0100

    Improved tests for JWT authentication routes

--- app/tests/routes/jwt_authentication/test_current_user.py
+++ app/tests/routes/jwt_authentication/test_current_user.py
@@ -1,34 +1,22 @@
 import pytest
-import pymongo
 from starlette.testclient import TestClient
 from fastapi import status
+from pytest import MonkeyPatch
 
-from app.config import JWTAuthenticationConfig, MongodbConnectionConfig
-from app.models import ResponseMessage, AWFAPIUser, AWFAPIRegisteredUser, \
-    E401Unauthorized
-from app.providers import AWFAPIUserProvider
-from app.services import JWTAuthenticationService, AWFAPIUserService
+from app.models import (ResponseMessage, AWFAPIUser, AWFAPIRegisteredUser,
+                        E401Unauthorized)
+from app.factories import MongoDBFactory, AWFAPIUserFactory, JWTAuthenticationFactory
 
 from app.routes import jwt_authentication as jwt_authentication_routes
 from app import oauth2_handlers
 
+from app.tests.fixtures.fixtures_entry_lists import awfapi_nonreadonly_user, awfapi_readonly_user
 from app.tests.fixtures.fixtures_tests import register_test_user, obtain_access_token, drop_collection
 
 
-mongodb_connection_string: str = MongodbConnectionConfig.get_db_connection_string()
-mongodb_collection_name: str = MongodbConnectionConfig.get_collection_name(test_suffix="_test")
-mongodb_engine: pymongo.MongoClient = pymongo.MongoClient(mongodb_connection_string)
-awfapi_user_provider: AWFAPIUserProvider = AWFAPIUserProvider(
-    connection_string=mongodb_connection_string,
-    collection_name=mongodb_collection_name,
-    db_engine=mongodb_engine
-)
-awfapi_user_service: AWFAPIUserService = AWFAPIUserService(awfapi_user_provider=awfapi_user_provider)
-jwt_authentication_service: JWTAuthenticationService = JWTAuthenticationService(
-    jwt_auth_config=JWTAuthenticationConfig.from_json(),
-    awfapi_user_provider=awfapi_user_provider,
-    awfapi_user_service=awfapi_user_service
-)
+mongodb_connection_string, mongodb_collection_name, mongodb_engine = MongoDBFactory.get_db_connection_details(test_suffix="_test")
+awfapi_user_provider, awfapi_user_service = AWFAPIUserFactory.get_provider_and_service(mongodb_connection_string, mongodb_collection_name, mongodb_engine)
+jwt_authentication_service = JWTAuthenticationFactory.get_service(awfapi_user_provider, awfapi_user_service)
 
 
 @pytest.fixture()
@@ -39,14 +27,13 @@ def client():
         yield test_client
 
 
-awfapi_nonreadonly_user: AWFAPIRegisteredUser = AWFAPIRegisteredUser(username="testuser", password="testpassword",
-                                                                     repeated_password="testpassword",
-                                                                     full_name="Test AWFAPIUserInput",
-                                                                     email="test.user@test.user", is_readonly=False)
-awfapi_readonly_user: AWFAPIRegisteredUser = AWFAPIRegisteredUser(username="testuser", password="testpassword",
-                                                                  repeated_password="testpassword",
-                                                                  full_name="Test AWFAPIUserInput",
-                                                                  email="test.user@test.user", is_readonly=True)
+def fixtures_before_test(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(jwt_authentication_routes, 'jwt_auth_service', jwt_authentication_service)
+    monkeypatch.setattr(oauth2_handlers, 'jwt_auth_service', jwt_authentication_service)
+
+
+def fixtures_after_test() -> None:
+    drop_collection(mongodb_engine, mongodb_collection_name)
 
 
 @pytest.mark.parametrize("awfapi_registered_user", [
@@ -57,8 +44,7 @@ def test_current_user_should_return_200_response(client, monkeypatch,
                                                  awfapi_registered_user: AWFAPIRegisteredUser) -> None:
     try:
         # Arrange
-        monkeypatch.setattr(jwt_authentication_routes, 'jwt_auth_service', jwt_authentication_service)
-        monkeypatch.setattr(oauth2_handlers, 'jwt_auth_service', jwt_authentication_service)
+        fixtures_before_test(monkeypatch)
         register_test_user(awfapi_user_service, awfapi_registered_user)
         access_token = obtain_access_token(client, awfapi_registered_user)
 
@@ -76,10 +62,10 @@ def test_current_user_should_return_200_response(client, monkeypatch,
         assert awfapi_user.is_readonly == awfapi_registered_user.is_readonly
 
     except Exception as e:
-        drop_collection(mongodb_engine, mongodb_collection_name)
+        fixtures_after_test()
         raise e
     else:
-        drop_collection(mongodb_engine, mongodb_collection_name)
+        fixtures_after_test()
 
 
 @pytest.mark.parametrize("awfapi_registered_user, access_token, expected_message", [
@@ -98,8 +84,7 @@ def test_current_user_should_return_401_response(client, monkeypatch,
                                                  expected_message: ResponseMessage) -> None:
     try:
         # Arrange
-        monkeypatch.setattr(jwt_authentication_routes, 'jwt_auth_service', jwt_authentication_service)
-        monkeypatch.setattr(oauth2_handlers, 'jwt_auth_service', jwt_authentication_service)
+        fixtures_before_test(monkeypatch)
         register_test_user(awfapi_user_service, awfapi_registered_user)
         if access_token is None:
             access_token = obtain_access_token(client, awfapi_registered_user)
@@ -116,7 +101,7 @@ def test_current_user_should_return_401_response(client, monkeypatch,
         assert message.code == expected_message.code
 
     except Exception as e:
-        drop_collection(mongodb_engine, mongodb_collection_name)
+        fixtures_after_test()
         raise e
     else:
-        drop_collection(mongodb_engine, mongodb_collection_name)
+        fixtures_after_test()
--- app/tests/routes/jwt_authentication/test_verify.py
+++ app/tests/routes/jwt_authentication/test_verify.py
@@ -1,33 +1,21 @@
 import pytest
-import pymongo
 from starlette.testclient import TestClient
 from fastapi import status
+from pytest import MonkeyPatch
 
-from app.config import JWTAuthenticationConfig, MongodbConnectionConfig
-from app.models import ResponseMessage, AWFAPIRegisteredUser, \
-    E401Unauthorized
-from app.providers import AWFAPIUserProvider
-from app.services import JWTAuthenticationService, AWFAPIUserService
+from app.models import (ResponseMessage, AWFAPIRegisteredUser,
+                        E401Unauthorized)
+from app.factories import MongoDBFactory, AWFAPIUserFactory, JWTAuthenticationFactory
 
 from app.routes import jwt_authentication as jwt_authentication_routes
 
+from app.tests.fixtures.fixtures_entry_lists import awfapi_readonly_user
 from app.tests.fixtures.fixtures_tests import register_test_user, obtain_access_token, drop_collection
 
 
-mongodb_connection_string: str = MongodbConnectionConfig.get_db_connection_string()
-mongodb_collection_name: str = MongodbConnectionConfig.get_collection_name(test_suffix="_test")
-mongodb_engine: pymongo.MongoClient = pymongo.MongoClient(mongodb_connection_string)
-awfapi_user_provider: AWFAPIUserProvider = AWFAPIUserProvider(
-    connection_string=mongodb_connection_string,
-    collection_name=mongodb_collection_name,
-    db_engine=mongodb_engine
-)
-awfapi_user_service: AWFAPIUserService = AWFAPIUserService(awfapi_user_provider=awfapi_user_provider)
-jwt_authentication_service: JWTAuthenticationService = JWTAuthenticationService(
-    jwt_auth_config=JWTAuthenticationConfig.from_json(),
-    awfapi_user_provider=awfapi_user_provider,
-    awfapi_user_service=awfapi_user_service
-)
+mongodb_connection_string, mongodb_collection_name, mongodb_engine = MongoDBFactory.get_db_connection_details(test_suffix="_test")
+awfapi_user_provider, awfapi_user_service = AWFAPIUserFactory.get_provider_and_service(mongodb_connection_string, mongodb_collection_name, mongodb_engine)
+jwt_authentication_service = JWTAuthenticationFactory.get_service(awfapi_user_provider, awfapi_user_service)
 
 
 @pytest.fixture()
@@ -38,10 +26,12 @@ def client():
         yield test_client
 
 
-awfapi_readonly_user: AWFAPIRegisteredUser = AWFAPIRegisteredUser(username="testuser", password="testpassword",
-                                                                  repeated_password="testpassword",
-                                                                  full_name="Test AWFAPIUserInput",
-                                                                  email="test.user@test.user", is_readonly=True)
+def fixtures_before_test(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(jwt_authentication_routes, 'jwt_auth_service', jwt_authentication_service)
+
+
+def fixtures_after_test() -> None:
+    drop_collection(mongodb_engine, mongodb_collection_name)
 
 
 @pytest.mark.parametrize("awfapi_registered_user, password, expected_message", [
@@ -59,7 +49,7 @@ def test_verify_should_return_200_response(client, monkeypatch,
                                            expected_message: ResponseMessage) -> None:
     try:
         # Arrange
-        monkeypatch.setattr(jwt_authentication_routes, 'jwt_auth_service', jwt_authentication_service)
+        fixtures_before_test(monkeypatch)
         register_test_user(awfapi_user_service, awfapi_registered_user)
         access_token = obtain_access_token(client, awfapi_registered_user)
 
@@ -75,10 +65,10 @@ def test_verify_should_return_200_response(client, monkeypatch,
         assert message.code == expected_message.code
 
     except Exception as e:
-        drop_collection(mongodb_engine, mongodb_collection_name)
+        fixtures_after_test()
         raise e
     else:
-        drop_collection(mongodb_engine, mongodb_collection_name)
+        fixtures_after_test()
 
 
 @pytest.mark.parametrize("awfapi_registered_user, password, expected_message", [
@@ -93,7 +83,7 @@ def test_verify_should_return_401_response(client, monkeypatch,
                                            expected_message: ResponseMessage) -> None:
     try:
         # Arrange
-        monkeypatch.setattr(jwt_authentication_routes, 'jwt_auth_service', jwt_authentication_service)
+        fixtures_before_test(monkeypatch)
         register_test_user(awfapi_user_service, awfapi_registered_user)
 
         # Act
@@ -106,7 +96,7 @@ def test_verify_should_return_401_response(client, monkeypatch,
         assert message.code == expected_message.code
 
     except Exception as e:
-        drop_collection(mongodb_engine, mongodb_collection_name)
+        fixtures_after_test()
         raise e
     else:
-        drop_collection(mongodb_engine, mongodb_collection_name)
+        fixtures_after_test()
